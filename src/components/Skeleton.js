6abe32163f113933791b3f6d1ee2a90301a5a023
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-07 13:38:00 +0530

    Animations with Tailwind CSS

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,6 +1,9 @@
 import classNames from 'classnames';
 
-function Skeleton({ times }) {
+function Skeleton({ times, className }) {
+    const outerClassNames = classNames('relative', 'overflow-hidden', 'bg-gray-200', 'rounded', 'mb-2.5', className);
+    const innerClassNames = classNames('animate-shimmer', 'absolute', 'inset-0', '-translate-x-full', 'bg-gradient-to-r', 'from-gray-200', 'via-white', 'to-gray-200');
+
     // const boxes = [];
     // for (let i = 0; i < times; i++) {
     //     boxes.push(<div key={i} />);
@@ -10,7 +13,11 @@ function Skeleton({ times }) {
     const boxes = Array(times)
         .fill(0)
         .map((_, i) => {
-            return <div key={i} />;
+            return (
+                <div key={i} className={outerClassNames}>
+                    <div className={innerClassNames} />
+                </div>
+            );
         });
     return boxes;
 }
